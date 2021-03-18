44c355fe75ad2020a5d4e56d3a5d15071de7517f
Vladyslav <vladyslav.oleksenko.1107@gmail.com>
2021-03-18 15:23:33 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,82 +1,31 @@
-const alphabet = [
-	'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
-	'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
-	'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
-	'э', 'ю', 'я',
-	'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
-	'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
-	'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь',
-	'Э', 'Ю', 'Я',
-	'і', 'ї', 'ґ', 'І', 'Ї', 'Ґ',
-	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-	'u', 'v', 'w', 'x', 'y', 'z',
-	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-	'U', 'V', 'W', 'X', 'Y', 'Z',
-	'@', '#', '$', '_', '&', '-', '+', '(', ')', '/',
-	'*', '"', "'", ':', ';', '!', '?', '.', ',', '=',
-	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-	' '
-];
+function encrypt (original, key) {
+	let encrypted = '';
 
-const table = createTable(alphabet);
+	for (let i in original) {
+		const originalSymbolCode = original.charCodeAt(i);
+		const keySymbolCode = key.charCodeAt(i % key.length);
 
+		let ecryptedSymbolCode = originalSymbolCode + keySymbolCode;
+		if (ecryptedSymbolCode >= 65536) ecryptedSymbolCode -= 65536;
 
-function createTable (alphabet) {
-	let table = [];
-	for (let i in alphabet) {
-		let line = alphabet.slice();
-
-		// move
-		let oldFirstElements = line.splice(0, i);
-		line = line.concat(oldFirstElements);
-
-		table[i] = line;
+		encrypted += String.fromCharCode(ecryptedSymbolCode);
 	}
 
-	return table;
+	return encrypted;
 }
 
-function encrypt (mess, key, table) {
-	let messEncrypted = '';
+function decrypt (original, key) {
+	let decrypted = '';
 
-	let alphabet = table[0];
+	for (let i in original) {
+		const originalSymbolCode = original.charCodeAt(i);
+		const keySymbolCode = key.charCodeAt(i % key.length);
 
-	for (let i in mess) {
-		let messSymbol = mess[i];
-		let keySymbol = key[i % key.length];
+		let decryptedSymbolCode = originalSymbolCode - keySymbolCode;
+		if (decryptedSymbolCode < 0) decryptedSymbolCode += 65536;
 
-		let messSymbolPosition = symbolPosition(messSymbol, alphabet);
-		let keySymbolPosition = symbolPosition(keySymbol, alphabet);
-		let messSymbolEncrypted = table[keySymbolPosition][messSymbolPosition];
-
-		messEncrypted += messSymbolEncrypted;
+		decrypted += String.fromCharCode(decryptedSymbolCode);
 	}
 
-	return messEncrypted;
-}
-
-function decrypt (mess, key, table) {
-	let messDecrypted = '';
-
-	let alphabet = table[0];
-
-	for (let i in mess) {
-		let messSymbol = mess[i];
-		let keySymbol = key[i % key.length];
-
-		let keySymbolPosition = symbolPosition(keySymbol, alphabet);
-		let messSymbolPosition = symbolPosition(messSymbol, table[keySymbolPosition]);
-		let messSymbolDecrypted = alphabet[messSymbolPosition];
-
-		messDecrypted += messSymbolDecrypted;
-	}
-
-	return messDecrypted;
-}
-
-function symbolPosition (symbol, array) {
-	for (let i in array)
-		if (array[i] == symbol) return i;
+	return decrypted;
 }
