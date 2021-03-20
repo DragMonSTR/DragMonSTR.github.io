f210bc20ed69d5ae485e62768edfb7a74669b416
Vladyslav <80710398+DragMonSTR@users.noreply.github.com>
2021-03-20 19:46:43 +0200

    changing1

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,7 @@
 const cardEl = getElByClassName("card");
 
-const keyInputEl = document.getElementById("key-input");
-const messInputEl = document.getElementById("mess-input");
+const keyInputEl = getElByClassName("key-input");
+const messInputEl = getElByClassName("mess-input");
 
 const encryptActionBtnEl = getElByClassName("action-btn", 0);
 const decryptActionBtnEl = getElByClassName("action-btn", 1);
@@ -10,6 +10,10 @@ const clearBtnEl = getElByClassName("clear-btn");
 
 const notificationBlockEl = getElByClassName("notification-block");
 const notificationEl = getElByClassName("notification");
+
+const showGuideBtnEl = getElByClassName("show-guide-btn");
+
+const encryptedStatusMessageEl = getElByClassName("encrypted-status-message");
 
 function getElByClassName (className, i = 0) {
 	return document.getElementsByClassName(className)[i];
@@ -26,6 +30,7 @@ messInputEl.addEventListener("blur", inputFieldsOnblur, false);
 
 function inputFieldsOnfocus () {
 	setTimeout(() => {
+			hideNotification();
 			cardEl.style.marginTop = "4vw";
 		}, 0);
 }
@@ -34,63 +39,6 @@ function inputFieldsOnblur () {
 	setTimeout(() => {
 			cardEl.style.marginTop = "7vw";
 		}, 0);
-}
-
-
-// change notification when typing in input fields
-keyInputEl.addEventListener("input", keyInputElOninput, false);
-messInputEl.addEventListener("input", messInputElOninput, false);
-
-function keyInputElOninput () {
-
-}
-
-function messInputElOninput () {
-	
-}
-
-function inputFieldsOninput () {
-	const keyLength = keyInputEl.value.length;
-	const messLength = messInputEl.value.length;
-
-	if (keyLength && messLength) {
-		encryptActionBtnEl.classList.remove("action-btn-unavailable");
-		decryptActionBtnEl.classList.remove("action-btn-unavailable");
-
-		changeNotificationType("accepted");
-		notificationTextEl.innerHTML = "Данные введены";
-	}
-	else {
-		encryptActionBtnEl.classList.add("action-btn-unavailable");
-		decryptActionBtnEl.classList.add("action-btn-unavailable");
-
-		if (notificationType == "tip") {
-			if (keyLength || messLength)
-				changeNotificationType("tip-light");
-		}
-		else if (notificationType == "tip-light") {
-			if (!keyLength && !messLength)
-				changeNotificationType("tip");
-		}
-
-		else if (notificationText == "Введите ключ") {
-			if (keyLength)
-				changeNotificationType("warning-light");
-			else
-				changeNotificationType("warning");
-		}
-		else if (notificationText == "Введите сообщение") {
-			if (messLength)
-				changeNotificationType("warning-light");
-			else
-				changeNotificationType("warning");
-		}
-
-		else if (notificationType == "accepted") {
-			changeNotificationType("tip");
-			notificationTextEl.innerHTML = "Как пользоваться?";
-		}
-	}
 }
 
 
@@ -118,10 +66,7 @@ clearBtnEl.addEventListener("click", clearBtnElOnclick, false);
 function clearBtnElOnclick () {
 	messInputEl.value = "";
 	messInputEl.focus();
-	resultBlockEl.style.display = "none";
 
-	changeNotificationType("tip");
-	notificationTextEl.innerHTML = "Как пользоваться?";
 	encryptActionBtnEl.classList.add("action-btn-unavailable");
 	decryptActionBtnEl.classList.add("action-btn-unavailable");
 }
@@ -135,39 +80,34 @@ function tryEncrypt () {
 	if (!checkFieldsValidity(true)) return;
 
 	const resultContent = encrypt(messInputEl.value, keyInputEl.value);
+	
+	copy(resultContent);
 
-	showResult("Зашифрованное сообщение:", resultContent);
+	showGuideBtnEl.style.display = "none";
+	encryptedStatusMessageEl.style.display = "block";
+
+	showNotification("success", "скопировано");
 }
 
 function tryDecrypt () {
 	if (!checkFieldsValidity(true)) return;
 
 	const resultContent = decrypt(messInputEl.value, keyInputEl.value);
-
-	showResult("Расшифрованное сообщение:", resultContent);
 }
 
 
 // additional functions
-function changeNotificationType (type) {
-	notificationType = type;
-
-	let backgroundColor;
-	if (type == "warning") backgroundColor = "#994161";
-	else if (type == "accepted") backgroundColor = "#a3c69a";
-
-	notificationBlockEl.style.backgroundColor = backgroundColor;
-}
-
-function checkFieldsValidity (needToshowNotification = false) {
+function checkFieldsValidity (changeDom = false) {
 	if (!keyInputEl.value.length) {
-		if (!needToshowNotification) return false;
+		if (!changeDom) return false;
+		keyInputEl.classList.add("wrong-input-field");
 		showNotification("warning", "Введите ключ!");
 		return false;
 	}
 
 	if (!messInputEl.value.length) {
-		if (!needToshowNotification) return false;
+		if (!changeDom) return false;
+		messInputEl.classList.add("wrong-input-field");
 		showNotification("warning", "Введите сообщение!");
 		return false;
 	}
@@ -178,7 +118,7 @@ function checkFieldsValidity (needToshowNotification = false) {
 function showNotification (notificationType, notificationText) {
 	if (notificationType == "warning")
 		notificationBlockEl.style.backgroundColor = "#994161";
-	else if (notificationType == "accepted")
+	else if (notificationType == "success")
 		notificationBlockEl.style.backgroundColor = "#a3c69a";
 
 	notificationEl.innerHTML = notificationText;
@@ -187,18 +127,15 @@ function showNotification (notificationType, notificationText) {
 	notificationBlockEl.style.bottom = "0";
 }
 
-function showResult (resultName, resultContent) {
-	resultBlockEl.style.display = "block";
-	resultNameEl.innerHTML = resultName;
-	resultContentEl.innerHTML = resultContent;
+function hideNotification () {
+	notificationBlockEl.style.display = "none";
+	notificationBlockEl.style.bottom = "-20vw";
+}
 
-	changeNotificationType("message");
-	notificationTextEl.innerHTML = "Скопировано";
-
-	// coping result to copy buffer
+function copy (text) {
 	let tmpEl = document.createElement('textarea');
 	document.getElementsByTagName('body')[0].append(tmpEl);
-	tmpEl.value = resultContent;
+	tmpEl.value = text;
 	tmpEl.select();
 	document.execCommand('copy');
 	tmpEl.remove();
